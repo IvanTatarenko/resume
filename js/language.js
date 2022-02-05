749433cf5dcd55011266bbf82df58601b1d934c0
Ivan Tatarenko <Vakummail@gmail.com>
2022-02-05 18:23:25 +0200

    Add: JS localization

---
+++ js/language.js
@@ -0,0 +1,56 @@
+// The locale our app first shows
+const defaultLocale = "uk";
+// The active locale
+let locale;
+// Gets filled with active locale translations
+let translations = {};
+// When the page content is ready...
+document.addEventListener("DOMContentLoaded", () => {
+    setLocale(defaultLocale);
+    bindLocaleSwitcher(defaultLocale);
+  });
+// Load translations for the given locale and translate
+// the page to this locale
+async function setLocale(newLocale) {
+  if (newLocale === locale) return;
+  const newTranslations = 
+    await fetchTranslationsFor(newLocale);
+  locale = newLocale;
+  translations = newTranslations;
+  translatePage();
+}
+// Retrieve translations JSON object for the given
+// locale over the network
+async function fetchTranslationsFor(newLocale) {
+  const response = await fetch(`/lang/${newLocale}.json`);
+  return await response.json();
+}
+// Replace the inner text of each element that has a
+// data-i18n-key attribute with the translation corresponding
+// to its data-i18n-key
+function translatePage() {
+  document
+    .querySelectorAll("[data-i18n-key]")
+    .forEach(translateElement);
+}
+// Replace the inner text of the given HTML element
+// with the translation in the active locale,
+// corresponding to the element's data-i18n-key
+function translateElement(element) {
+  const key = element.getAttribute("data-i18n-key");
+  const translation = translations[key];
+  const translation_title = translations[key + ".title"];
+  const translation_alt = translations[key + ".alt"];
+  element.innerText = translation;
+  element.title = translation_title;
+  element.alt = translation_alt;
+}
+function bindLocaleSwitcher(initialValue) {
+  const switcher = 
+    document.querySelector("[data-i18n-switcher]");
+  switcher.value = initialValue;
+  switcher.onchange = (e) => {
+    // Set the locale to the selected option[value]
+    setLocale(e.target.value);
+  };
+}
